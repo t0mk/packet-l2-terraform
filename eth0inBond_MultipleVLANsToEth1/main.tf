a071891de32450735e8da9c3925487c986ec49e6
Tomas Karasek <tom.to.the.k@gmail.com>
2019-02-25 18:16:56 +0200

    add C1 and C2

---
+++ eth0inBond_MultipleVLANsToEth1/main.tf
@@ -0,0 +1,45 @@
+locals {
+    fac = "nrt1"
+}
+
+resource "packet_project" "test" {
+    name = "testpro"
+}
+
+resource "packet_device" "test" {
+    hostname         = "test"
+    plan             = "s1.large.x86"
+    facility         = "${local.fac}"
+    operating_system = "ubuntu_16_04"
+    billing_cycle    = "hourly"
+    project_id       = "${packet_project.test.id}"
+}
+
+resource "packet_vlan" "test2" {
+  description = "VLAN1"
+  facility    = "${local.fac}"
+  project_id  = "${packet_project.test.id}"
+}
+
+resource "packet_vlan" "test2" {
+  description = "VLAN1"
+  facility    = "${local.fac}"
+  project_id  = "${packet_project.test.id}"
+}
+
+resource "packet_vlan_attachment" "test" {
+  device_id = "${packet_device.test.id}"
+  # 2nd port is eth1
+  port_id = "${packet_device.test.ports.2.id}"
+  vlan_id = "${packet_vlan.test1.id}"
+}
+
+resource "packet_vlan_attachment" "test" {
+  device_id = "${packet_device.test.id}"
+  # 2nd port is eth1
+  port_id = "${packet_device.test.ports.2.id}"
+  vlan_id = "${packet_vlan.test2.id}"
+}
+
+
+
---
+++ eth0inBond_SingleVLANtoEth1/main.tf
@@ -0,0 +1,33 @@
+locals {
+    fac = "nrt1"
+}
+
+resource "packet_project" "test" {
+    name = "testpro"
+}
+
+resource "packet_device" "test" {
+    hostname         = "test"
+    plan             = "s1.large.x86"
+    facility         = "${local.fac}"
+    operating_system = "ubuntu_16_04"
+    billing_cycle    = "hourly"
+    project_id       = "${packet_project.test.id}"
+}
+
+resource "packet_vlan" "test" {
+  description = "VLAN1"
+  facility    = "${local.fac}"
+  project_id  = "${packet_project.test.id}"
+}
+
+resource "packet_vlan_attachment" "test" {
+  device_id = "${packet_device.test.id}"
+  # 2nd port is eth1
+  port_id = "${packet_device.test.ports.2.id}"
+  vlan_id = "${packet_vlan.test.id}"
+}
+
+
+
+
