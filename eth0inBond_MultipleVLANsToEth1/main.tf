34f1debe72c6482569e1ebccea56af8bba6e215c
Tomas Karasek <tom.to.the.k@gmail.com>
2019-02-25 18:22:29 +0200

    fix typos

--- eth0inBond_MultipleVLANsToEth1/main.tf
+++ eth0inBond_MultipleVLANsToEth1/main.tf
@@ -15,26 +15,26 @@ resource "packet_device" "test" {
     project_id       = "${packet_project.test.id}"
 }
 
-resource "packet_vlan" "test2" {
+resource "packet_vlan" "test1" {
   description = "VLAN1"
   facility    = "${local.fac}"
   project_id  = "${packet_project.test.id}"
 }
 
 resource "packet_vlan" "test2" {
-  description = "VLAN1"
+  description = "VLAN2"
   facility    = "${local.fac}"
   project_id  = "${packet_project.test.id}"
 }
 
-resource "packet_vlan_attachment" "test" {
+resource "packet_vlan_attachment" "test1" {
   device_id = "${packet_device.test.id}"
   # 2nd port is eth1
   port_id = "${packet_device.test.ports.2.id}"
   vlan_id = "${packet_vlan.test1.id}"
 }
 
-resource "packet_vlan_attachment" "test" {
+resource "packet_vlan_attachment" "test2" {
   device_id = "${packet_device.test.id}"
   # 2nd port is eth1
   port_id = "${packet_device.test.ports.2.id}"
